d95ca99ba449a5d33c8593cc7f5d3a094a0f9c70
Ercan <kadri503394@gmail.com>
2024-05-27 23:57:59 +0300

    feat: added Team component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import About from '@/components/home/About';
+import Blog from '@/components/home/Blog';
+import Brands from '@/components/home/Brands';
+import Classes from '@/components/home/Classes';
+import Hero from '@/components/home/Hero';
+import Membership from '@/components/home/Membership';
+import Team from '@/components/home/Team';
+import Testimonial from '@/components/home/Testimonial';
+
 export default function Home() {
-  return <main>homepage</main>;
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Classes />
+      <Team />
+      <Membership />
+      <Testimonial />
+      <Blog />
+      <Brands />
+      {/* temporary div */}
+      {/* <div className="h-[3000px]"></div> */}
+    </main>
+  );
 }
